c937b09f4466d97291dfde945fbac8b446b853d0
Tanmoy Bir <tanmoy7468@gmail.com>
2023-12-25 21:06:27 +0600

    emplement theme mode toggle a button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,52 +1,72 @@
 import { NavLink } from "react-router-dom";
+import { IoMdMoon, IoMdSunny } from "react-icons/io";
+import { useTheme } from "../../hooks/useTheme";
 
 const Navbar = () => {
+    const {themeChanced, mode} = useTheme()
+
+
     const navLinks = [
         {
             id: 1,
             link: '/',
-            name: 'Home'
+            name: 'HOME'
         },
         {
             id: 2,
             link: '/cars',
-            name: 'Cars'
+            name: 'CARS'
         },
         {
             id: 3,
             link: '/about',
-            name: 'About'
+            name: 'ABOUT'
         },
         {
             id: 4,
             link: '/booking',
-            name: 'Booking'
+            name: 'BOOKING'
         }
     ]
     return (
-        <div>
-            <nav className="flex justify-between">
-                <div className="">
-                    <h1>Cars Rental</h1>
-                </div>
-                <ul className="flex items-center gap-5">
+        <nav className="shadow-md bg-white dark:bg-dark dark:text-white">
+            <div className="container">
+                <div className="flex items-center justify-between">
+                    {/* Logo */}
+                    <div className="text-3xl font-bold font-serif">
+                        <h1>Cars Rental</h1>
+                    </div>
+                    {/* NavLinks */}
+                    <div className="hidden md:block">
+                        <ul className="flex items-center gap-5">
+                            {
+                                navLinks.map(data => (
+                                    <li className="py-4" key={data.id}>
+                                        <NavLink
+                                            to={data.link}
+                                            className={({ isActive, isPending }) =>
+                                                isPending ? "pending" : isActive ?
+                                                    "py-2 border-b-2 text-blue-00 border-blue-200 transition-colors duration-500 text-lg font-medium" :
+                                                    "py-2 hover:border-b-2 hover:text-yellow-200 hover:border-yellow-200 transition-colors duration-500 text-lg font-medium"
+                                            }
+                                        >
+                                            {data.name}
+                                        </NavLink>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                    {/* Mood */}
+                    <div className="" onClick={themeChanced}>
                     {
-                        navLinks.map(data => (
-                            <li key={data.id}>
-                                <NavLink
-                                    to={data.link}
-                                    className={({ isActive, isPending }) =>
-                                        isPending ? "pending" : isActive ? "active" : ""
-                                    }
-                                >
-                                    {data.name}
-                                </NavLink>
-                            </li>
-                        ))
+                        mode === 'dark'? <IoMdSunny/> : <IoMdMoon/>
                     }
-                </ul>
-            </nav>
-        </div>
+                    </div>
+                </div>
+            </div>
+
+        </nav>
     );
 };
 
